9672ee397a553afc47b0c26f421b3525c44f577c
Crigilmar <crigilmar@gmail.com>
2017-05-16 09:24:27 +0100

    Se añadieron los atributos cantidad y nombre

---
+++ gitgui1/Ingredientes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitgui1;
+
+/**
+ *
+ * @author alumnociclo
+ */
+public class Ingredientes {
+   
+  private String cantidad;
+  private String nombre;
+  
+    
+    
+}
