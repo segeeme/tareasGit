ca8f7acc8269140327704e8e346d8ff0c62d001a
Crigilmar <crigilmar@gmail.com>
2017-05-16 09:17:40 +0100

    Constructores; uno solo con el nombre y otro con todos los campos ya creados

--- gitgui1/Receta.java
+++ gitgui1/Receta.java
@@ -14,6 +14,17 @@ public class Receta {
    private String nombre;
    private String ingredientes;
    private String preparacion;
+   
+   Receta(){
+       
+   }
+    
+    Receta(String nombre,String ingredientes,String preparacion){
+      this.nombre=nombre;
+      this.ingredientes=ingredientes;
+      this.preparacion=preparacion;
+    }
+    
     
     
     
